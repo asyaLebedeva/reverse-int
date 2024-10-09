d1a21679deb546e3fd0ef61a508c9665e962cd30
Asya <asyafrontend@gmail.com>
2024-10-09 19:34:51 +0300

    feat: add code to function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let inputNum = n.toString();
+    let enterNum = inputNum.split('');
+    let arr = [];
+    arr = enterNum.reverse();
+    let result = arr.join('');
+    return parseInt(result);
+};
